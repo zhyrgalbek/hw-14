e1539c57ff4fa17fc3835f1682feaca8a721458b
zhyrgalbek <zhyrgalbek.kamalov@gmail.com>
2022-05-07 12:10:55 +0300

    add clenup function

--- src/Components/kvadrat/Kvadrat.js
+++ src/Components/kvadrat/Kvadrat.js
@@ -7,15 +7,19 @@ function Kvadrat(){
     const [styles, setStyles] = useState('');
     const [count, setCount] = useState(0);
     useEffect(()=>{
-        setTimeout(()=>{
+        const timer = setTimeout(()=>{
             setCount(count + 1);
             if(count >= arr.length - 1){
                 setCount(0);
             }            
-            // console.log(count)
             setStyles(arr[count]);
             divClass.current.className = `kvadrat ${styles}`;
+            // console.log('changed');
         }, 1000);
+        return ()=>{
+            // console.log('clear')
+            clearTimeout(timer);
+        }
     }, [styles]);
     return <div className="kvadrat" ref={divClass} >Hello world</div>
 }
